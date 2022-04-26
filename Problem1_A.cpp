1207f862bb42f38ebd4b391033efb2f31146989c
BobdaFett <lvas463@gmail.com>
2022-04-26 12:39:40 -0400

    Add project files.

---
+++ Problem1_A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// This program may not use the library class string or vectors.
+
+void main() {
+	// Something here...
+	/*
+	* Input several lines of text from the user as a C-string.
+	* Print an alphabetical listing of each word in the text and how many times that word occurred without regard for upper/lowercase.
+	* 
+	* It may be assumed that there is no punctuation, and that exactly one space separates each word.
+	* The number of characters per line along with the total number of lines may be assumed but clearly stated. 
+	*/
+
+	// Hint given: keep a pointer to each word within a char* array, or keep them inside of a 2-dimensional array.
+	// This will require strtok()
+}
