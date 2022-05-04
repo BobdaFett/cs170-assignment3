3adee8fc989c7c82b911329a2af0f2aba857c6b6
BobdaFett <lvas463@gmail.com>
2022-05-04 08:29:44 -0400

    Started redoing problem 1.

--- Problem1_A.cpp
+++ Problem1_A.cpp
@@ -1,66 +1,26 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-#pragma warning(disable : 4996)
+// Lucas Vas
+// This class may not use #include <string>
 
-// This program may not use the library class string or vectors.
+char* getTokens(char inputString[]);
+void sortTokens(char* tokens);
 
 int main() {
-	/*
-	* Input several lines of text from the user as a C-string.
-	* Print an alphabetical listing of each word in the text and how many times that word occurred without regard for upper/lowercase.
-	* 
-	* It may be assumed that there is no punctuation, and that exactly one space separates each word.
-	* The number of characters per line along with the total number of lines may be assumed but clearly stated. 
-	*/
+	// Get input from user
+	char userInput[1000] = {};
+	cout << "Please input a string, using ctrl + Z to end:" << endl;
+	cin.get(userInput, '\x1A');
 
-	// Hint given: keep a pointer to each word within a char* array, or keep the words themselves inside of a 2-dimensional array.
-	// Add these pointers through a function maybe?
-	// This will require strtok() - this will return a sequence of characters.
-	char userInput[100]{};
+	// Get tokens from full string (dynamic list return)
+	char* allTokens = getTokens(userInput);
 
-	cout << "Please input a line of text, no longer than 100 characters long: ";
-	cin.getline(userInput, sizeof(userInput)); // <- pulls 100 characters from cin... would take more if the array was larger.
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	// TODO: validate inputs, terminate at end of file.
+	// Sort tokens by first letter in every word (function)
+	sortTokens(allTokens);
 
-	cout << "Splitting this string into tokens between spaces..." << endl;
-	char* strTokens[5] = {};
-	strTokens[0] = strtok(userInput, " ");
-	for (int i = 1; i < 5; i++) {
-		strTokens[i] = strtok(NULL, " ");
-	}
-
-	for (char* i : strTokens) {
-		cout << i << endl;
-	}
-
-	// Get the total number of tokens inside of the input string, multiple lines included:
-	int numTokens = 0;
-	for (char i : userInput) {
-		// This is not actually activating. Are spaces an escape character?
-		if (' ' == i) numTokens++;
-		else {
-			printf("This was false - character \"%c\" was not a space.\n", i);
-		}
-	}
-	cout << "Total number of tokens: " << numTokens << endl;
-	// Instead of printing these, store them inside of an array.
-	//while (strTokens != NULL) {
-
-	//}
+	// Print all of the tokens one by one (with the number of times each of them happens)
+	// Debating if the sortTokens function should also print these out.
 
 	return 0;
-}
-
-void getTokens(char* inputStr, char *tokens[]) {
-	// If i were to add extra values to the array, each time i would have to create a new array, copy everything over, then delete the array.
-
-}
-
-void noteTaking() {
-	fstream fileObject;
-	fileObject.open("nameHere.txt", ios::in | ios::out | ios::trunc);
-	fileObject.seekg(0, ios::beg);
 }
