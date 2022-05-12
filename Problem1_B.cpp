e37bb4536820f4ccd9192640d6630dbda453ef33
BobdaFett <lvas463@gmail.com>
2022-05-12 17:25:04 -0400

    Most of problem 1B works. Still can't figure out the sorting methods.

--- Problem1_B.cpp
+++ Problem1_B.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 #pragma warning(disable : 4996)
@@ -12,38 +14,61 @@ const int LINE_MAX = 1000;
 
 char** getTokens(char inputString[], const int numTokens);
 int cmpTokens(const void* a, const void* b);
+char* inputToCstring(string inputString);
 
 int main() {
 	// Get input from user
-	char userInput[LINE_MAX] = {};
-	cout << "Please input a string, maxiumum length 1000 characters, using ctrl + Z to end:" << endl;
-	cin.getline(userInput, LINE_MAX, cin.eof()); // this technically shouldn't work, but it kinda does? Must use ^Z on its own line.
+	string userInput, inputLine;
+	int numTokens = 0;
+	while (true) {
+		cout << "Please input the name of the text file, including the file extension.\n"
+			<< "The name must be all one word, with no spaces: ";
+		cin >> userInput;
 
-	int numOfTokens = 0;
-	for (int i = 0; i < strlen(userInput); i++)
-		if (userInput[i] == ' ' || userInput[i] == '\n')
-			numOfTokens++;
+		ifstream userFile;
+		userFile.open(userInput, ios::in | ios::out);
 
-	// Get tokens from full string (dynamic list return)
-	char** allTokens = getTokens(userInput, numOfTokens + 1);
-	qsort(allTokens, (size_t)numOfTokens + 1, (size_t)sizeof(char), cmpTokens);
+		// get each line of characters from the file, then add it to a string.
+		string temp;
+		if (userFile.good()) {
+			while (!userFile.eof()) {
+				userFile >> temp;
+				inputLine += temp + ' ';
+				numTokens++;
+			}
+			break;
+		}
+		else {
+			cout << "File could not be opened. Please enter a new file name.\n\n";
+		}
+	}
 
-	for (int i = 0; i < strlen(userInput); i++)
+	cout << "Contents of file \"" << userInput << "\":\n\n"
+		<< inputLine << endl;
+
+	// Convert inputLine into a C-String
+	char* inputCString = new char[inputLine.length() + 1];
+	strcpy(inputCString, inputLine.c_str());
+
+	char** allTokens = getTokens(inputCString, numTokens);
+
+	// As of right now, this doesn't sort anything. I don't know how to write the compare function properly.
+	qsort(allTokens, numTokens, sizeof(char*), cmpTokens);
+	
+	// Print every token afterwards.
+	for (int i = 0; i < numTokens; i++)
 		cout << allTokens[i];
-
-	// Print all of the tokens one by one (with the number of times each of them happens)
-	// Debating if the sortTokens function should also print these out.
 
 	return 0;
 }
 
-char** getTokens(char inputString[], const int numTokens) {
+char** getTokens(char* inputString, const int numTokens) {
 	char** tokensList = new char* [numTokens];
 	char* temp = strtok(inputString, " "); // It is assumed that there is no punctuation
 	int currentIndex = 0;
 	while (temp != NULL) {
 		tokensList[currentIndex++] = temp; // Could overrun, however I don't think the program will reach that point. Unless something else increments currentIndex.
-		cout << temp << endl;
+		/*cout << temp << endl;*/
 		temp = strtok(NULL, " ");
 	}
 	return tokensList;
@@ -51,9 +76,10 @@ char** getTokens(char inputString[], const int numTokens) {
 
 // Not at all sure where I went wrong here. Any pointers would be helpful.
 int cmpTokens(const void* a, const void* b) {
-	// Will compare each token to each other and then swap based on number that is returned.
+	// Should compare each token to each other and then swap based on number that is returned.
 	const char* arg1 = (const char*)a;
 	const char* arg2 = (const char*)b;
 
-	return *(char*)arg1 - *(char*)arg2;
+	// I know that this needs to be a lot more complex, but i'm not quite sure the logic behind it.
+	return *(char*)arg1[1] - *(char*)arg2[1];
 }
